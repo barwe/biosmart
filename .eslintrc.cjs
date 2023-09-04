39dae6ca87d35eb6341247bc7bc7acd2202a5bc6
barwe <995488247@qq.com>
2023-09-04 18:01:05 +0800

    run lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "plugin:vue/vue3-essential",
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "vue",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  env: { browser: true, es2021: true },
+  extends: 'plugin:vue/vue3-essential',
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'vue/multi-word-component-names': 0,
+  },
 }
