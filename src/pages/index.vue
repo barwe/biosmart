051682416bd1d83e21c5987b3e458415105b914e
barwe <995488247@qq.com>
2023-09-04 11:28:38 +0800

    init: naive-ui

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { NButton } from 'naive-ui'
 import { useThemeStore } from '@/store/site/theme'
 
 const count = ref(0)
@@ -8,7 +9,7 @@ const themeStore = useThemeStore()
 
 <template>
   <div>
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <n-button @click="count++">count is {{ count }}</n-button>
     <div>{{ themeStore.theme }}</div>
   </div>
 </template>
